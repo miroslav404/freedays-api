8ea99b9d2d9aab273e6165da957d1a752f85f8b8
Miroslav Radanovic <miroslav.radanovic@undabot.com>
2018-05-03 15:49:04 +0200

    Initial commit

---
+++ src/main/kotlin/com/billcosby/freedaysapi/FreedaysApiApplication.kt
@@ -0,0 +1,11 @@
+package com.billcosby.freedaysapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FreedaysApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<FreedaysApiApplication>(*args)
+}
